aa2d4edc735401354ace696733b87ff5b981a56d
解　鵬彦 <53389139+xiepy@users.noreply.github.com>
2025-05-02 04:17:04 +0900

    Add silakka54 keymap
    Add silakka54 keymap for myself

---
+++ keyboards/silakka54/config.h
@@ -0,0 +1,34 @@
+/* Copyright 2024 Juho T. (@Squalius-cephalus)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT_POLL 10
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_WATCHDOG_TIMEOUT 3000
+
+#define RGB_MATRIX_DEFAULT_VAL 20
+#define RGBLIGHT_LIMIT_VAL 20
+#define MOUSEKEY_MOVE_DELTA 3
---
+++ keyboards/silakka54/keymaps/vial/keymap.c
@@ -0,0 +1,73 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum custom_layers {
+    _BASE,
+    _FN1,
+    _FN2,
+    _FN3,
+};
+
+#define KC_FN1 MO(_FN1)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_INT1,
+                                            KC_LALT, KC_SPC, KC_GRV,           KC_ENT,  KC_SPC,  KC_FN1
+    ),
+    [1] = LAYOUT(
+        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_MINS,  KC_EQL,
+        KC_F10,  KC_F11, KC_F12, KC_UP, KC_TRNS, KC_DELETE,                                KC_TRNS, MS_BTN1, MS_UP, MS_BTN1,  KC_LBRC,  KC_RBRC,
+        KC_LSFT,  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,                            MS_WHLU, MS_LEFT, MS_DOWN,   MS_RGHT, KC_QUOT, KC_NUHS,
+        KC_LCTL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            MS_WHLD, MS_BTN1, MS_BTN2, KC_TRNS, KC_TRNS, KC_INT3,
+                                            KC_LALT, KC_LGUI, KC_MYCM,           KC_WBAK,  KC_WFWD,  KC_TRNS
+    )
+};
+
+HSV my_colors_hsv[] = {
+    {HSV_WHITE},
+    {HSV_GREEN},
+    {HSV_RED},
+    {HSV_ORANGE},
+    {HSV_CYAN},
+    {HSV_PURPLE},
+    {HSV_YELLOW},
+    {HSV_PINK},
+    {HSV_GOLDENROD},
+    {HSV_CORAL},
+    {HSV_TEAL},
+    {HSV_MAGENTA},
+    {HSV_SPRINGGREEN},
+    {HSV_GOLD},
+    {HSV_CHARTREUSE},
+    {HSV_BLACK}
+};
+
+
+layer_state_t default_layer_state_set_user(layer_state_t layer_state) {
+    uint8_t layer = biton32(layer_state);
+    // hsv version.
+    HSV my_color_hsv = my_colors_hsv[layer];
+    rgblight_sethsv(my_color_hsv.h, my_color_hsv.s, my_color_hsv.v);
+    return layer_state;
+}
+
+layer_state_t layer_state_set_user(layer_state_t layer_state) {
+    uint8_t layer = biton32(layer_state);
+    // rgb_led_t struct default for ws2812 uses grb.
+    // rgb_led_t my_color = my_colors[layer];
+    // rgblight_setrgb_at(my_color.r, my_color.g, my_color.b, 0);
+    // ws2812_init();
+    // hsv version.
+    HSV my_color_hsv = my_colors_hsv[layer];
+    rgblight_sethsv(my_color_hsv.h, my_color_hsv.s, my_color_hsv.v);
+    // rgblight_sethsv_at(my_color_hsv.h, my_color_hsv.s, my_color_hsv.v, 0);
+    return layer_state;
+}
+
